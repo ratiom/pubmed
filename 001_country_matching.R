50e7687ab9d33e67f1c40dc333dd96f8c524528a
ratiom <skippydoesntknow@gmail.com>
2014-07-23 20:02:51 -0400

    Promising double loop in 003

--- 001_country_matching.R
+++ 001_country_matching.R
@@ -1,8 +1,19 @@
-query.aff.search <- data.frame()
-df <- for(i in query.affiliation){
-        if(grepl("Italy", i) == 1) {
-                query.aff.search <- rbind(query.aff.search, i)
-        }
-        #else print("K")#query.aff.search <- rbind(query.aff.search, "Not Italy")
-        
-}
+cou <- c("Algeria", "Italy")
+affil <- c("1 place in, Algeria.", "2 place in Italy.", "3 place in, Ireland.", "4 Place in, Algeria.")
+
+#returns true/false matrix if cou in affil
+sapply(cou, grepl, affil)
+
+#returns true/false lists for each element in affil
+lapply(cou, grepl, affil)
+
+#definitely loops through the list of affs, and gives true/false for every country
+country.test <- lapply(query.affiliation, function(x) sapply(country.list, grepl, x))
+
+#returns only true values, but not the value
+#sub(pattern, replacement, text)
+country.test <- sapply(query.affiliation, function(x) sub(pattern = country.list, replacement = any(sapply(country.list, grepl, x))))
+
+#places counrty into a list
+test1 <- if(grepl(cou[1], affil[1]) == TRUE) sub(affil[1], cou, cou[1])
+
---
+++ 003_country_matching_test.R
@@ -0,0 +1,11 @@
+test1 <- c()
+count = 1
+para <- for(i in affil[count]){
+                for(j in cou){
+                        if(grepl(j,i) == TRUE){
+                                count <- count + 1
+                                test1 <- paste(test1,i)
+                        }
+                }
+}
+
