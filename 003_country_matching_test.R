64787a61d0bba38870275890db51c095509f5df9
ratiom <skippydoesntknow@gmail.com>
2014-07-23 20:07:01 -0400

    looks like this works for 003

--- 003_country_matching_test.R
+++ 003_country_matching_test.R
@@ -1,10 +1,10 @@
 test1 <- c()
 count = 1
-para <- for(i in affil[count]){
+para <- for(i in affil){
                 for(j in cou){
                         if(grepl(j,i) == TRUE){
                                 count <- count + 1
-                                test1 <- paste(test1,i)
+                                test1 <- paste(test1,j)
                         }
                 }
 }
